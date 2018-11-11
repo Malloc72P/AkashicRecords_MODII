a6af258393510cbc1a22cdb08ea024e14f2d499e
dase3 <dase3@DESKTOP-56BFANV>
2018-11-11 20:39:48 +0900

    Hybrid_ReactAndServlet 음...... 결론부터 말할께 실패작이야
    작동은 제대로 한다만, 라우팅이 의미가 없다.
    앞으로가기, 뒤로가기는 작동한다만, 이 주소를 가지고 페이지를 찾을 수 없어.
    리엑트 앱에서나 의미가 있지, 정작 이 요청을 톰캣서버가 받게되니까 이동을 못하더라고.
    
    그러니까 노드서버를 올려야해.
    
    흐음... 이거 좀 문제네

---
+++ src/main/webapp/reactApp/precache-manifest.5b61d5567642d6c4c36b10d705327fa4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3686c82c573f59c604b74e1198c49998",
+    "url": "/AkashicRecords/reactApp/static/media/worker.3686c82c.png"
+  },
+  {
+    "revision": "ee3c7f321cc90e10b0d971e45f387088",
+    "url": "/AkashicRecords/reactApp/static/media/warmind2.ee3c7f32.jpg"
+  },
+  {
+    "revision": "788fc904243580b1a23b",
+    "url": "/AkashicRecords/reactApp/static/js/runtime~main.788fc904.js"
+  },
+  {
+    "revision": "4b8f02131b65d9d02449",
+    "url": "/AkashicRecords/reactApp/static/js/main.4b8f0213.chunk.js"
+  },
+  {
+    "revision": "4b60746c997cbb61b8e3",
+    "url": "/AkashicRecords/reactApp/static/js/1.4b60746c.chunk.js"
+  },
+  {
+    "revision": "4b8f02131b65d9d02449",
+    "url": "/AkashicRecords/reactApp/static/css/main.fb736a5a.chunk.css"
+  },
+  {
+    "revision": "af389b3e536c4e6825487e2a5d683464",
+    "url": "/AkashicRecords/reactApp/index.html"
+  }
+];
